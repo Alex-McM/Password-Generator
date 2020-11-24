68b87148c708ef5ed100790df66112ab61f950fb
El Psy Congroo <mcmasterjalexander@gmail.com>
2020-11-24 14:40:59 +1100

    Arrays Listed

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment Code //
-var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+
+
 
 // Write password to the #password input //
 function writePassword() {
@@ -14,6 +17,11 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Arrays //
+var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "+"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = ["a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 
 // Varable's Declared //
