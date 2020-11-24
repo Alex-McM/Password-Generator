76ac27dfb741d64701c99a0da7785dd2335758e9
El Psy Congroo <mcmasterjalexander@gmail.com>
2020-11-24 14:07:38 +1100

    JS Pseudo Initial

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+// Assignment Code //
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input //
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Arrays //
+
+
+// Varable's Declared //
+
+// Prompt for user to select how many characters that are going to use //
+
+// Loop when outside parameters 8-128 characters //
+
+// Alert user chosen charcter amount //
+
+// Password Parameters //
+
+// Loop if password is outside parameters 8-128 //
+
+// Assigned action to password parameters //
+
+// Empty string will be filled according to for loop selecting random characters from array //
+
+// Display password in the pass word input box //
